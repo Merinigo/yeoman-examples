c7aca90b9e7439fbbfda7a823faa0ddfd613682b
Iñigo <merinigo@gmail.com>
2018-04-03 23:05:26 +0200

    Métodos privados y helpers
    Estos métodos no se ejecutaran automaticamente

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,8 +2,23 @@ var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
 
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.helperMethod = function() { //No se llama automaticamente
+            console.log("Helper method!");
+        };
+    }
+
     method1(){
         console.log("HOLA YEOMAN!");
+
+        this.helperMethod();
+        this._private_method();
+    }
+
+    _private_method(){ //No se llama automaticamente
+        console.log("Private method!");
     }
 
 };
