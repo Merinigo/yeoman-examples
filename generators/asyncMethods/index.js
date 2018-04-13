b89b13b5549ed27065d4f228491ac77af7df2703
Iñigo <merinigo@gmail.com>
2018-04-13 19:50:51 +0200

    AsyncMethodsv1

--- generators/asyncMethods/index.js
+++ generators/asyncMethods/index.js
@@ -3,7 +3,20 @@ var Generator = require('yeoman-generator');
 module.exports = class extends Generator {
 
     asynTask() {
+        var done = this.async();
 
+        console.log("startTask");
+
+        setTimeout(function(){
+            console.log("taskDone");
+            done();
+        }, 1000);
+
+        console.log("waiting...");
+    }
+
+    writing() {
+        console.log("writingMethod");
     }
 
 }
